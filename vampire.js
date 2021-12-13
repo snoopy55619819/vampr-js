93654fe07041eb96c1e768ab56ca900ffefb645e
snoopy55619819 <nikhiltallapureddy@gmail.com>
2021-12-13 09:18:50 +0000

    examples for tree traversal

--- vampire.js
+++ vampire.js
@@ -10,22 +10,37 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+    }
+    return currentVampire.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let currentVampire = this;
+    let countParentNodes = 0;
 
+    while (currentVampire.creator) {
+      countParentNodes += 1;
+      currentVampire = currentVampire.creator;
+    }
+    return countParentNodes;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    const thisVampireDecendents = this.numberOfVampiresFromOriginal;
+    const otherVampireDecendents = vampire.numberOfVampiresFromOriginal;
+    return (thisVampireDecendents < otherVampireDecendents);
   }
 
   /** Stretch **/
@@ -36,7 +51,26 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    if (this === vampire) {
+      return this;
+    }
+    const youngerVampireParents = {};
+    let youngerVampire = (this.isMoreSeniorThan(vampire) ? vampire : this);
+    let olderVampire = (this.isMoreSeniorThan(vampire) ? this : vampire);
 
+    while (youngerVampire.creator) {
+      youngerVampireParents[youngerVampire.creator.name] = youngerVampire.creator.name;
+      youngerVampire = youngerVampire.creator;
+    }
+    const condition = true;
+    while (condition) {
+      if (youngerVampireParents[olderVampire.name]) {
+        return olderVampire;
+      } else if (!olderVampire.creator) {
+        return olderVampire;
+      }
+      olderVampire = olderVampire.creator;
+    }
   }
 }
 
